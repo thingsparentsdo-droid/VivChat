6028c7ff65ba7bcc2a94d1e4eee71671bfb07080
VivAlgorithm1 <thingsparentsdo@gmail.com>
2025-11-18 17:29:52 -0800

    Add Server and Client V1

---
+++ Serverchat.py
@@ -0,0 +1,78 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import requests
+import random
+import json
+
+def fruitget(fruit):
+    with open("Fruit.json") as f:
+        data = json.load(f)
+        para = [data["Fruits"][fruit]["color"],data["Fruits"][fruit]["shape"],data["Fruits"][fruit]["plant"]]
+        return "Color: " + para[0] + ", Shape: " + para[1] + ", Plant: " + para[2]
+class FruitData(BaseModel):
+    fruit: str
+
+
+chatserver = FastAPI()
+
+@chatserver.post("/fruitdata")
+
+def fruitdata(data: FruitData):
+    fruit = data.fruit
+    returndata = fruitget(fruit)
+    return returndata
+
+
+@chatserver.get("/test")
+
+def test():
+    return {"test":True}
+
+@chatserver.get("/shakesperantrashtalk")
+
+def shakesperantrashtalk():
+    
+    trash = ["pepper stained dung beetle","eddiwigeing explosive paprica blast storm","denominating expert baha bird","bestowing pizza ballon","experimental parataptat piptin cat","expert testamonial trash talker","gobgob pepel breasted comnom brained bibleflunk","nobodys buisness buisnessman coverd in beeswax","plunging ferret footed pilgrim","forcibly fed faratat disasrer disaster"]
+    return("thou art a " + random.choice(trash))
+
+
+
+@chatserver.get("/404")
+
+def error404():
+    return "error 404 Page Not Found"
+
+@chatserver.get("/scrape")
+
+def scrape(url):
+    return requests.get(url)
+
+
+
+@chatserver.get("/DndGroupChatInbox")
+def Messages():
+    with open("Chat.json") as f:
+        data = json.load(f)
+        payload = {
+            "Dnd_Group_Chat": data["Dnd_Group_Chat"]
+        }
+        return payload
+    
+class MessageData(BaseModel):
+    sender: str
+    Message: str
+
+@chatserver.post("/DndGroupChatSendMessage")
+def DndGroupChatSendMessage(data: MessageData):
+    with open("Chat.json") as f:
+        datajson = json.load(f)
+        newmessage = {"sender":data.sender,"Message":data.Message}
+        datajson["Dnd_Group_Chat"].append(newmessage)
+    with open("Chat.json","w") as f:
+        json.dump(datajson,f,indent=4)
+
+
+    
+
+
+
---
+++ clientchat.py
@@ -0,0 +1,37 @@
+import requests
+import random
+import os
+import json
+URL = 'http://127.0.0.1:8000'
+os.system("cls")
+def get(CHAT):
+    if CHAT == "Dnd_Group_Chat":
+        response = requests.get(f"{URL}/DndGroupChatInbox")
+        response = response.json()["Dnd_Group_Chat"]
+        returntxt = ''
+        returntxt += "--- Dnd Group Chat ---\n"
+        for i in response:
+            returntxt += "("
+            returntxt += i['Message']
+            returntxt += ")\n"
+            returntxt += "|/\n  "
+            returntxt += i["sender"] + "\n\n\n"
+
+        return returntxt
+    else:
+        return "Chat not found"
+
+name = input("Enter your name: ")
+while True:
+    os.system("cls")
+    CHAT = "Dnd_Group_Chat"
+    print(get(CHAT))
+
+    txt = input()
+    if txt == "exit":
+        break  
+    elif txt == '':
+        pass
+    else:
+        message = {"sender":name,"Message":txt}
+        response = requests.post(f"{URL}/DndGroupChatSendMessage",json=message)
